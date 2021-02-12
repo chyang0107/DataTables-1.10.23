62f45b1f5ebc233ec003937d6a4dfca2ac2f3bf2
chyang0107 <chyang0107@gmail.com>
2021-02-12 13:05:32 -0600

    new way to add link

--- js/dataTable.campsite.js
+++ js/dataTable.campsite.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-    // $('a[data-toggle="tab"]').on( 'shown.bs.tab', function (e) {
-    //     $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
-    // });
-
-    $(document).on('shown.bs.tab', 'a[data-toggle="tab"]', function (e) {
-        $.fn.dataTable.tables({ visible: true, api: true }).columns.adjust();
+    $('a[data-toggle="tab"]').on( 'shown.bs.tab', function (e) {
+        $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
     });
     
-    $('#example').DataTable({
+    var ajaxOKParkData = {
+        campsite :{ajax: "./data/dataCampsiteFeb092021.txt"},
+        building: {ajax: "./data/dataBuildingFeb2021.txt"}
+    }
+
+
+    var commonSettings = {
         language: {
             searchBuilder: {
                 title: ' Data Filter'
             }
         },
         buttons: [ 'searchBuilder' ],
-        "ajax": {url: "./data/dataCampsiteFeb092021.txt",
-                 dataSrc:'data'   
-        },
         "deferRender": true,
         "dom": 'Qr<"wrapper"Blipt>',
         buttons: [
@@ -24,17 +23,46 @@ $(document).ready(function() {
                 text: 'PDF',
                 extend: 'print',
                 title: 'Hello',
-                messageTop: 'This print was produced using the Print button for DataTables'
+                messageTop: 'The print button is to print the query by your browser'
             }
         ],
         "scrollY": 800,
         "scrollX": true
-    });
-    $('#exampleBuilding').DataTable({
-        "ajax": {url: "./data/dataBuildingFeb2021.txt",
-                 dataSrc:'data'   
-        },
+    };
+    
+    var dataCampsite = Object.assign(commonSettings, ajaxOKParkData['campsite'])
+    $('#example').DataTable(dataCampsite)
+    var dataBuilding = Object.assign(commonSettings,ajaxOKParkData['building'])
+    $('#exampleBuilding').DataTable(dataBuilding)
+
+    // $('#example').DataTable({
+    //     language: {
+    //         searchBuilder: {
+    //             title: ' Data Filter'
+    //         }
+    //     },
+    //     buttons: [ 'searchBuilder' ],
+    //     "ajax": {url: "./data/dataCampsiteFeb092021.txt",
+    //              dataSrc:'data'   
+    //     },
+    //     "deferRender": true,
+    //     "dom": 'Qr<"wrapper"Blipt>',
+    //     buttons: [
+    //         'csv',{
+    //             text: 'PDF',
+    //             extend: 'print',
+    //             title: 'Hello',
+    //             messageTop: 'This print was produced using the Print button for DataTables'
+    //         }
+    //     ],
+    //     "scrollY": 800,
+    //     "scrollX": true
+    // });
+    // $('#exampleBuilding').DataTable({
+    //     "ajax": {url: "./data/dataBuildingFeb2021.txt",
+    //              dataSrc:'data'   
+    //     },
         
-    });
+    // });
 
 });
